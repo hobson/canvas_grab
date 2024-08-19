23fa318a398a41e86b56966f6dce6d3a1a750e59
hobs <github@totalgood.com>
2024-08-19 16:03:57 -0700

    use virtualenv instead of venv

--- canvas_grab.sh
+++ canvas_grab.sh
@@ -6,17 +6,17 @@ python3 --version
 
 echo "Check virtual environment..."
 
-if [ ! -d "venv" ]; then
+if [ ! -d ".venv" ]; then
     echo "Create virtual environment..."
-    python3 -m venv venv
+    python3 -m virtualenv .venv
     echo "Activate virtual environment..."
-    . venv/bin/activate
-    echo "Install dependencies with SJTUG mirror..."
-    python -m pip install --upgrade pip -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
-    python -m pip install -r requirements.txt -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
-else
-    echo "Activate virtual environment..."
-    . venv/bin/activate
 fi
+
+echo "Activate virtual environment..."
+source ./.venv/bin/activate
+
+echo "Install dependencies with SJTUG mirror..."
+python -m pip install --upgrade pip -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
+python -m pip install -r requirements.txt -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
 
 python main.py $@
